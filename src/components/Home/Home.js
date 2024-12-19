12c0042648c08a9dff7b6c13dff7530c213789a1
James Albritton <james@jamesalbritton.com>
2024-12-18 19:54:22 -0500

    support mobile

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
 
 const HomeContainer = styled.div`
   background-color: #572932;
   color: #DFD1A7;
   padding: 4rem 2rem;
+  text-align: center;
+`;
+
+const MobileDiv = styled.div`
+  background-color: #572932;
+  color: #DFD1A7;
+  padding: 0px 0px 0px 0px;
   text-align: center;
 `;
 
@@ -35,6 +43,23 @@ const Button = styled.a`
   }
 `;
 
+const MobileButton = styled.a`
+  background-color: #DFD1A7;
+  color: #333;
+  padding: 1rem 1.5rem;
+  font-size: 1.25rem;
+  text-decoration: none;
+  border-radius: 5px;
+  width: 250px;
+  height: 60px;
+  text-align: center;
+  display: inline-block;
+  margin: 15px;
+  &:hover {
+    background-color: #ffaa00;
+  }
+`;
+
 const MainImage = styled.img`
   width: 35%;
   height: auto;
@@ -43,16 +68,49 @@ const MainImage = styled.img`
 `;
 
 function Home() {
-    const imageUrl = 'https://s3.amazonaws.com/jjalbritton.media/pics/cropped.jpg';
+  const [isMobile, setIsMobile] = useState(false);
+  const imageUrl = 'https://s3.amazonaws.com/jjalbritton.media/pics/cropped.jpg';
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768); // Define the threshold for mobile devices
+    };
+    
+    // Initial check
+    checkMobile();
+    
+    // Event listener for resizing
+    window.addEventListener('resize', checkMobile);
+    
+    // Clean up on component unmount
+    return () => {
+      window.removeEventListener('resize', checkMobile);
+    };
+  }, []);
+
   return (
     <HomeContainer>
-         <MainImage src={imageUrl} alt="Main performance image" />
-      <Title>J.J. Albritton</Title>
-      <Subtitle>Singer, guitarist, solo entertainer in Bradenton, FL.</Subtitle>
-      <Subtitle>941-209-8340</Subtitle>
-      <Subtitle>jj@jjalbritton.com</Subtitle>
-      <Button href="https://youtu.be/AeQWwXxScQs">Demo Video</Button>
-      <Button href="/calendar">View My Calendar</Button>
+      {isMobile ? (
+        <MobileDiv>
+          <MainImage src={imageUrl} alt="Main performance image" />
+          <Title>J.J. Albritton</Title>
+          <Subtitle>Singer, guitarist, solo entertainer in Bradenton, FL.</Subtitle>
+          <Subtitle>941-209-8340</Subtitle>
+          <Subtitle>jj@jjalbritton.com</Subtitle>
+          <MobileButton href="https://youtu.be/AeQWwXxScQs">Demo Video</MobileButton>
+          <MobileButton href="/calendar">View My Calendar</MobileButton>
+        </MobileDiv>
+      ) : (
+        <>
+          <MainImage src={imageUrl} alt="Main performance image" />
+          <Title>J.J. Albritton</Title>
+          <Subtitle>Singer, guitarist, solo entertainer in Bradenton, FL.</Subtitle>
+          <Subtitle>941-209-8340</Subtitle>
+          <Subtitle>jj@jjalbritton.com</Subtitle>
+          <Button href="https://youtu.be/AeQWwXxScQs">Demo Video</Button>
+          <Button href="/calendar">View My Calendar</Button>
+        </>
+      )}
     </HomeContainer>
   );
 }
